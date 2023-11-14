7b267c7923df57b48c320aee7bcad1c191af74c4
Jakub <117046128+KLesiu@users.noreply.github.com>
2023-11-14 14:52:53 +0100

    Add styles to buttons and add quote

--- Components/Preload.tsx
+++ Components/Preload.tsx
@@ -2,26 +2,28 @@ import { Text,Image,View,ImageBackground,TouchableOpacity } from "react-native";
 import logoLGYM from './img/logoLGYM.png'
 import backgroundLGYM from './img/backgroundLGYMApp500.png'
 import { PreloadStyles } from "./styles/PreloadStyles";
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from "./Login";
-const Stack = createNativeStackNavigator();
+import {useState,useEffect} from 'react'
+
 
 const Preload=()=>{
+    const [quote,setQuote]=useState<boolean>(false)
+    useEffect(()=>{
+        setTimeout(()=>setQuote(true),10000)
+    },[])
     return(
         <View>
-            
-                <ImageBackground source={backgroundLGYM}>
+            <ImageBackground source={backgroundLGYM}>
                         <View style={PreloadStyles.preLoadDiv}>
                             <View style={PreloadStyles.preLoadContainer}>
                                 <Image source={logoLGYM} style={PreloadStyles.logoLGYMAPP}/>
-                                <TouchableOpacity>
-                                    <Text>LOGIN</Text>
+                                <TouchableOpacity style={PreloadStyles.login}>
+                                    <Text style={PreloadStyles.loginText}>LOGIN</Text>
                                 </TouchableOpacity>
-                                <TouchableOpacity>
-                                    <Text>REGISTER</Text>
+                                <TouchableOpacity style={PreloadStyles.register}>
+                                    <Text style={PreloadStyles.registerText}>REGISTER</Text>
                                 </TouchableOpacity>
-                                    
+                                {quote?<Text>'Strength does not come from winning. Your struggles develop your strengths. When you go through hardships and decide not to surrender, that is strength. When you make an impasse passable, that is strength. But you must have ego, the kind of ego which makes you think of yourself in terms of superlatives. You must want to be the greatest. We are all starved for compliments. So we do things that get positive feedback.' (Arnold Schwarzenegger, 1982)</Text>:''}
                             </View>
                                 
                         </View>
--- Components/styles/PreloadStyles.tsx
+++ Components/styles/PreloadStyles.tsx
@@ -6,7 +6,7 @@ export const PreloadStyles = StyleSheet.create({
         height:'40%'
     },
     preLoadDiv:{
-        backgroundColor: 'rgba(92, 92, 92,0.9)',
+        backgroundColor: 'rgba(92, 92, 92,0.1)',
         height:'100%',
         width:'100%',
 
@@ -23,5 +23,47 @@ export const PreloadStyles = StyleSheet.create({
         alignItems:'center',
         backgroundColor: 'rgba(0, 0, 0, 0.897)',
         justifyContent: 'space-around',
+    },
+    login:{
+        alignItems: 'center',
+        backgroundColor: 'rgb(170, 180, 189)',
+        borderRadius: 10,
+        display: 'flex',
+        justifyContent: 'center',
+        marginTop: 20,
+        height: '10%',
+        opacity: 1,
+        width: '70%',
+    },
+    register:{
+        alignItems: 'center',
+        backgroundColor: 'rgb(170, 180, 189)',
+        borderRadius: 10,
+        display: 'flex',
+        justifyContent: 'center',
+        marginTop: 20,
+        height: '10%',
+        opacity: 1,
+        width: '70%',
+    },
+    loginText:{
+        color: 'black',
+        fontSize: 25,
+        fontWeight: '700',
+        textDecorationLine: 'none',
+        textShadowColor: 'rgb(0, 0, 0)',
+        textShadowOffset: { width: 1, height: 1 },
+        textShadowRadius: 1,
+        letterSpacing: 4,  
+    },
+    registerText:{
+        color: 'black',
+        fontSize: 25,
+        fontWeight: '700',
+        textDecorationLine: 'none',
+        textShadowColor: 'rgb(0, 0, 0)',
+        textShadowOffset: { width: 1, height: 1 },
+        textShadowRadius: 1,
+        letterSpacing: 4,  
     }
 })
