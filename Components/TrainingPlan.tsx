f28b067daf6258a6857352d917955182537332e5
Jakub <117046128+KLesiu@users.noreply.github.com>
2023-11-14 22:09:57 +0100

    Add withoutPlanContainer at plan section

--- Components/TrainingPlan.tsx
+++ Components/TrainingPlan.tsx
@@ -1,11 +1,37 @@
 import { Text,Image,View,ImageBackground,TouchableOpacity } from "react-native";
 import { TrainingPlanStyles } from "./styles/TrainingPlanStyles";
 import backgroundLogo from './img/backgroundLGYMApp500.png'
-import {useState} from 'react'
+import {useState,useEffect} from 'react'
+import { useFonts,Teko_700Bold } from "@expo-google-fonts/teko";
+import {Caveat_400Regular} from '@expo-google-fonts/caveat';
+import * as SplashScreen from 'expo-splash-screen'
 const TrainingPlan:React.FC=()=>{
     const [yourPlan,setYourPlan]=useState<JSX.Element>(<View style={TrainingPlanStyles.withoutPlanContainer}>
-        <Text>You dont have any plan</Text>
+        <Text style={{fontFamily:'Caveat_400Regular',...TrainingPlanStyles.withoutPlanText}}>You dont have any plans!</Text>
+        <TouchableOpacity style={TrainingPlanStyles.withoutPlanButton}>
+            <Text style={{fontFamily:'Caveat_400Regular',...TrainingPlanStyles.withoutPlanButtonText}}>Create your plan now!</Text>
+        </TouchableOpacity>
     </View>)
+    const [fontsLoaded]=useFonts({
+        Teko_700Bold,
+        Caveat_400Regular
+    })
+    useEffect(() => {
+        const loadAsyncResources = async () => {
+          try {
+            SplashScreen.preventAutoHideAsync();
+            await fontsLoaded;
+            SplashScreen.hideAsync();
+          } catch (error) {
+            console.error('Błąd ładowania zasobów:', error);
+          }
+        };
+    
+        loadAsyncResources();
+      }, [fontsLoaded]);
+      if(!fontsLoaded){
+        return <View><Text>Loading...</Text></View>
+    }
     return(
         <View style={TrainingPlanStyles.sectionPlan} >
             <ImageBackground style={TrainingPlanStyles.backgroundIMG} source={backgroundLogo}>
--- Components/styles/TrainingPlanStyles.tsx
+++ Components/styles/TrainingPlanStyles.tsx
@@ -7,12 +7,13 @@ export const TrainingPlanStyles = StyleSheet.create({
         
     },
     withoutPlanContainer:{
+        alignItems:'center',
         display:'flex',
         flexDirection:'column',
         justifyContent:'flex-start',
         height:'100%',
         width:'100%',
-        backgroundColor:'rgba(255,255,255,0.9)',
+        backgroundColor:'rgba(255,255,255,0.97)',
         borderRadius:10,
     },
     backgroundIMG:{
@@ -26,4 +27,23 @@ export const TrainingPlanStyles = StyleSheet.create({
         marginRight:'1%',
         height:'100%'
     },
+    withoutPlanText:{
+        fontSize:30,
+        textAlign:'center'
+    },
+    withoutPlanButton:{
+        backgroundColor: 'rgb(134,134,134)',
+        width:'50%',
+        height:'10%',
+        display:'flex',
+        alignItems:'center',
+        justifyContent:'center',
+        borderRadius:10,
+        marginTop:'5%'
+    },
+    withoutPlanButtonText:{
+        fontSize:20,
+        width:'100%',
+        textAlign:'center'
+    }
 })
