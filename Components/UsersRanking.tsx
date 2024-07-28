f12613ee5cdba3c318d86f7b7eb3174b821ed6c0
Jakub Leśkiewicz <kubaxl222@gmail.com>
2024-07-28 23:27:54 +0200

    fix styles

--- Components/UsersRanking.tsx
+++ Components/UsersRanking.tsx
@@ -2,10 +2,11 @@ import React, { useEffect, useState } from "react";
 import { View, Text, ScrollView } from "react-native";
 import { UserRanking } from "./types/Training";
 import ViewLoading from "./ViewLoading";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const UsersRanking: React.FC = () => {
   const [ranking, setRanking] = useState<UserRanking[]>([]);
-  const username = localStorage.getItem("username")
+  const [username,setUsername]= useState<string>("");
   const getRanking = async () => {
     const response = await fetch(
       "https://lgym-app-api-v2.vercel.app/api/getBestTenUsersFromElo"
@@ -13,6 +14,9 @@ const UsersRanking: React.FC = () => {
       .then((res) => res.json())
       .catch((err) => err);
     setRanking(response);
+    const username = await AsyncStorage.getItem("username");
+    if(!username)return
+    setUsername(username)
   };
 
   useEffect(() => {
@@ -27,14 +31,14 @@ const UsersRanking: React.FC = () => {
       >
        Ranking
       </Text>
-      <ScrollView className="flex flex-col py-4 gap-2 smh:h-40 xsmh:h-72 mdh:h-80 lgh:h-96 ">
+      <ScrollView className="flex flex-col py-4 gap-2 smh:h-52 mdh:h-64  ">
       {ranking.length ? (
         ranking.map((ele: UserRanking) => {
             let color = "text-white"
             if(ele.user.name === username) color="text-[#4CD964]"
           return (
             
-            <View className="flex flex-row ">
+            <View className="flex flex-row " key={ele.position}>
               <Text
                 className={color + " mr-2"}
                 
@@ -54,6 +58,7 @@ const UsersRanking: React.FC = () => {
       ) : (
         <ViewLoading />
       )}
+      <View className="h-10 w-full"></View>
       </ScrollView>
     </View>
   );
