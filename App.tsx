26aa47c8cb9811321314627f4096535f67b5a3af
Jakub Leśkiewicz <kubaxl222@gmail.com>
2024-10-06 13:59:12 +0200

    updt

--- App.tsx
+++ App.tsx
@@ -1,21 +1,20 @@
-import React, { useEffect } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Preload from './Components/Preload';
-import Login from './Components/Login';
-import Register from './Components/Register';
-import Home from './Components/Home';
+import React, { useEffect } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Preload from "./Components/Preload";
+import Login from "./Components/Login";
+import Register from "./Components/Register";
+import Home from "./Components/Home";
 import {
   OpenSans_400Regular,
   OpenSans_700Bold,
   OpenSans_300Light,
   useFonts,
 } from "@expo-google-fonts/open-sans";
-import { Caveat_400Regular } from '@expo-google-fonts/caveat';
+import { Caveat_400Regular } from "@expo-google-fonts/caveat";
 import * as SplashScreen from "expo-splash-screen";
 import { NativeWindStyleSheet } from "nativewind";
-import { BackHandler, Alert ,StatusBar } from 'react-native';
-
+import { BackHandler, Alert, StatusBar } from "react-native";
 
 NativeWindStyleSheet.setOutput({
   default: "native",
@@ -23,19 +22,19 @@ NativeWindStyleSheet.setOutput({
 
 const Stack = createNativeStackNavigator();
 
-const App:React.FC = () => {
+const App: React.FC = () => {
   const [fontsLoaded] = useFonts({
     OpenSans_400Regular,
     OpenSans_700Bold,
     OpenSans_300Light,
-    Caveat_400Regular
+    Caveat_400Regular,
   });
   useEffect(() => {
     const backAction = () => {
-      Alert.alert("Uwaga!", "Przycisk wstecz jest zablokowany na tym ekranie.");
-       return true;
+      Alert.alert("Attention!", "The back button is blocked on this screen.");
+      return true;
     };
-    StatusBar.setBackgroundColor('#121212')
+    StatusBar.setBackgroundColor("#121212");
     StatusBar.setBarStyle("light-content");
     const backHandler = BackHandler.addEventListener(
       "hardwareBackPress",
@@ -53,16 +52,32 @@ const App:React.FC = () => {
         console.error("Błąd ładowania zasobów:", error);
       }
     };
-  
+
     loadAsyncResources();
   }, [fontsLoaded]);
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Preload">
-        <Stack.Screen name="Preload" component={Preload}  options={{ headerShown: false }} />
-        <Stack.Screen name="Login" component={Login } options={{ headerShown: false }}/>
-        <Stack.Screen name='Register' component={Register}  options={{ headerShown: false }}/>
-        <Stack.Screen name='Home' component={Home}  options={{ headerShown: false }}/>
+        <Stack.Screen
+          name="Preload"
+          component={Preload}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Register"
+          component={Register}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
