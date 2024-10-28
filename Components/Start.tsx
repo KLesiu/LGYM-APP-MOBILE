f48878b16b6d30f21b5ccbcb42209c247c6f116d
Jakub Leśkiewicz <kubaxl222@gmail.com>
2024-10-28 23:13:50 +0100

    Training summary

--- Components/Start.tsx
+++ Components/Start.tsx
@@ -14,8 +14,7 @@ import { Message } from "./enums/Message";
 
 const Start: React.FC<StartProps> = (props) => {
   const apiURL = `${process.env.REACT_APP_BACKEND}`;
-  const [lastTrainingInfo, setLastTrainingInfo] =
-    useState<LastTrainingInfo>();
+  const [lastTrainingInfo, setLastTrainingInfo] = useState<LastTrainingInfo>();
   const [userInfo, setUserInfo] = useState<UserInfo>();
   const [error, setError] = useState<string>("");
   const [progress, setProgress] = useState<number>();
@@ -23,43 +22,43 @@ const Start: React.FC<StartProps> = (props) => {
   useEffect(() => {
     initStart();
   }, []);
-  const initStart = async()=>{
+  const initStart = async () => {
     setViewLoading(true);
     await getLastTrainingInfo();
     await getRankInfo();
     setViewLoading(false);
-  }
+  };
   const getLastTrainingInfo = async () => {
     try {
       const id = await AsyncStorage.getItem("id");
       const response = await fetch(`${apiURL}/api/${id}/getLastTraining`);
-  
+
       if (!response.ok) {
         // If response status is not in the 200 range
         console.error("Failed to fetch last training info:", response.status);
         return setError(Message.DidntFind);
       }
-  
+
       const data: ErrorMsg | LastTrainingInfo = await response.json();
       if ("msg" in data) return setError(data.msg);
-  
+
       setLastTrainingInfo(data);
     } catch (error) {
       console.error("Network or JSON parsing error:", error);
       setError(Message.DidntFind);
     }
   };
-  
+
   const getRankInfo = async () => {
     try {
       const id = await AsyncStorage.getItem("id");
       const response = await fetch(`${apiURL}/api/${id}/getUserInfo`);
-  
+
       if (!response.ok) {
         console.error("Failed to fetch rank info:", response.status);
         return setError("Failed to fetch rank info.");
       }
-  
+
       const data: UserInfo = await response.json();
       setUserInfo(data);
       setProgress(Math.floor((data.elo / data.nextRank.needElo) * 10000) / 100);
@@ -73,8 +72,11 @@ const Start: React.FC<StartProps> = (props) => {
   };
   return (
     <View className="w-full flex flex-col flex-1 bg-[#121212] ">
-      <View style={{gap:8}} className="flex h-full w-full flex-col">
-        <View style={{borderRadius:8}} className="flex w-full justify-between flex-row bg-[#1E1E1E73] items-center p-4 ">
+      <View style={{ gap: 8 }} className="flex h-full w-full flex-col">
+        <View
+          style={{ borderRadius: 8 }}
+          className="flex w-full justify-between flex-row bg-[#1E1E1E73] items-center p-4 "
+        >
           <View>
             <Text
               className="text-[#94e798] text-lg"
@@ -82,35 +84,40 @@ const Start: React.FC<StartProps> = (props) => {
             >
               Last Training:
             </Text>
-            {!error && lastTrainingInfo && Object.keys(lastTrainingInfo).length  ?
-                          <View className="flex">
-                          <Text
-                            className="text-white text-sm"
-                            style={{ fontFamily: "OpenSans_400Regular" }}
-                          >
-                            Date:{" "}
-                            {new Date(lastTrainingInfo.createdAt!).toLocaleString()}
-                          </Text>
-                          <Text
-                            className="text-white text-sm"
-                            style={{ fontFamily: "OpenSans_400Regular" }}
-                          >
-                            Type: {lastTrainingInfo.planDay.name}
-                          </Text>
-                        </View>
-            :
+            {!error &&
+            lastTrainingInfo &&
+            Object.keys(lastTrainingInfo).length ? (
+              <View className="flex">
+                <Text
+                  className="text-white text-sm"
+                  style={{ fontFamily: "OpenSans_400Regular" }}
+                >
+                  Date: {new Date(lastTrainingInfo.createdAt!).toLocaleString()}
+                </Text>
+                <Text
+                  className="text-white text-sm"
+                  style={{ fontFamily: "OpenSans_400Regular" }}
+                >
+                  Type: {lastTrainingInfo.planDay.name}
+                </Text>
+              </View>
+            ) : (
               <Text
                 className="text-white text-sm"
                 style={{ fontFamily: "OpenSans_400Regular" }}
               >
                 {error}
               </Text>
-             }
+            )}
           </View>
 
           <Pressable
-            onPress={() => navigateTo(<AddTraining toggleMenuButton={props.toggleMenuButton} />)}
-            style={{borderRadius:12}}
+            onPress={() =>
+              navigateTo(
+                <AddTraining toggleMenuButton={props.toggleMenuButton} />
+              )
+            }
+            style={{ borderRadius: 12 }}
             className="h-16 w-24 bg-[#94e798]  flex items-center justify-center"
           >
             <Text
@@ -121,7 +128,10 @@ const Start: React.FC<StartProps> = (props) => {
             </Text>
           </Pressable>
         </View>
-        <View style={{borderRadius:8}} className="flex w-full flex-row justify-between bg-[#1E1E1E73] items-center p-4">
+        <View
+          style={{ borderRadius: 8 }}
+          className="flex w-full flex-row justify-between bg-[#1E1E1E73] items-center p-4"
+        >
           <View>
             <Text
               className="text-[#94e798] text-lg"
@@ -163,9 +173,14 @@ const Start: React.FC<StartProps> = (props) => {
               </Text>
             </View>
           </View>
-          {userInfo && userInfo.profileRank && <ProfileRank rank={userInfo.profileRank} />}
+          {userInfo && userInfo.profileRank && (
+            <ProfileRank rank={userInfo.profileRank} />
+          )}
         </View>
-        <View style={{borderRadius:12}} className="flex w-full justify-between flex-row bg-[#1E1E1E73] items-center p-4  flex-1">
+        <View
+          style={{ borderRadius: 12 }}
+          className="flex w-full justify-between flex-row bg-[#1E1E1E73] items-center p-4  flex-1"
+        >
           <UsersRanking />
         </View>
       </View>
--- Components/TrainingSummary.tsx
+++ Components/TrainingSummary.tsx
@@ -5,6 +5,7 @@ import {
   ImageProps,
   TouchableOpacity,
   Pressable,
+  ScrollView,
 } from "react-native";
 import { useEffect, useState } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
@@ -45,224 +46,234 @@ const TrainingSummary: React.FC<TrainingSummaryProps> = (props) => {
       >
         Training Summary
       </Text>
-      {/* <Pressable
-        style={{ borderRadius: 8 }}
-        className="w-[100px] h-[50px] p-[5px] absolute top-0 right-0 mr-[10px] border-white border-[1px] border-solid "
-        onPress={() => props.closePopUp()}
+      <ScrollView
+        className="w-full min-h-[300px] -mr-4 pr-4"
+        contentContainerStyle={{
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+          gap: 16,
+        }}
       >
-        <Text
-          className="text-white text-[30px] text-center"
-          style={{
-            fontFamily: "Teko_700Bold",
-          }}
+        <View
+          style={{ borderRadius: 8, gap: 16 }}
+          className="flex w-full flex-col  justify-between items-center"
         >
-          Close
-        </Text>
-      </Pressable> */}
-      <View
-        style={{ borderRadius: 8,gap:16 }}
-        className="flex w-full flex-col  justify-between items-center"
-      >
-        <View className=" bg-[#1E1E1E73] w-full  p-4">
-          <View style={{ gap: 8 }} className="flex flex-row items-center">
+          <View className=" bg-[#1E1E1E73] w-full  p-4">
+            <View style={{ gap: 8 }} className="flex flex-row items-center">
+              <Text
+                className="text-[#94e798] text-lg"
+                style={{ fontFamily: "OpenSans_700Bold" }}
+              >
+                Best progress:
+              </Text>
+              <Text
+                className="text-white text-base"
+                style={{ fontFamily: "OpenSans_400Regular" }}
+              >
+                {bestProgress.exercise}
+              </Text>
+            </View>
+
+            <View style={{ gap: 8 }} className="flex flex-col  justify-around">
+              <Text
+                className="text-white text-sm"
+                style={{ fontFamily: "OpenSans_400Regular" }}
+              >
+                Series number: {bestProgress.series}
+              </Text>
+              <View style={{ gap: 8 }} className="flex flex-row">
+                <Text
+                  className="text-white text-sm"
+                  style={{ fontFamily: "OpenSans_400Regular" }}
+                >
+                  Reps:
+                </Text>
+                {bestProgress.repsScore >= 0 ? (
+                  <Text
+                    className="text-[#94e798] text-sm"
+                    style={{ fontFamily: "OpenSans_400Regular" }}
+                  >
+                    +{bestProgress.repsScore}
+                  </Text>
+                ) : (
+                  <Text
+                    className="text-red-400 text-sm"
+                    style={{ fontFamily: "OpenSans_400Regular" }}
+                  >
+                    {bestProgress.repsScore}
+                  </Text>
+                )}
+              </View>
+              <View style={{ gap: 8 }} className="flex flex-row">
+                <Text
+                  className="text-white text-sm"
+                  style={{ fontFamily: "OpenSans_400Regular" }}
+                >
+                  Weight:
+                </Text>
+                {bestProgress.weightScore >= 0 ? (
+                  <Text
+                    className="text-[#94e798] text-sm"
+                    style={{ fontFamily: "OpenSans_400Regular" }}
+                  >
+                    +{bestProgress.weightScore}kg
+                  </Text>
+                ) : (
+                  <Text
+                    className="text-red-400 text-sm"
+                    style={{ fontFamily: "OpenSans_400Regular" }}
+                  >
+                    {bestProgress.weightScore}kg
+                  </Text>
+                )}
+              </View>
+            </View>
+          </View>
+          <View className=" bg-[#1E1E1E73] w-full  p-4">
+            <View style={{ gap: 8 }} className="flex flex-row items-center">
+              <Text
+                className="text-red-400 text-lg"
+                style={{ fontFamily: "OpenSans_700Bold" }}
+              >
+                Worst Regress:{" "}
+              </Text>
+              <Text
+                className="text-white text-base"
+                style={{ fontFamily: "OpenSans_400Regular" }}
+              >
+                {worseRegress.exercise}
+              </Text>
+            </View>
+            <View style={{ gap: 8 }} className="flex flex-col  justify-around">
+              <Text
+                className="text-white text-sm"
+                style={{ fontFamily: "OpenSans_400Regular" }}
+              >
+                Series number: {worseRegress.series}
+              </Text>
+              <View style={{ gap: 8 }} className="flex flex-row">
+                <Text
+                  className="text-white text-sm"
+                  style={{ fontFamily: "OpenSans_400Regular" }}
+                >
+                  Reps:
+                </Text>
+                {worseRegress.repsScore >= 0 ? (
+                  <Text
+                    className="text-[#94e798] text-sm"
+                    style={{ fontFamily: "OpenSans_400Regular" }}
+                  >
+                    +{worseRegress.repsScore}
+                  </Text>
+                ) : (
+                  <Text
+                    className="text-red-400 text-sm"
+                    style={{ fontFamily: "OpenSans_400Regular" }}
+                  >
+                    {worseRegress.repsScore}
+                  </Text>
+                )}
+              </View>
+              <View style={{ gap: 8 }} className="flex flex-row">
+                <Text
+                  className="text-white text-sm"
+                  style={{ fontFamily: "OpenSans_400Regular" }}
+                >
+                  Weight:
+                </Text>
+                {worseRegress.weightScore >= 0 ? (
+                  <Text
+                    className="text-[#94e798] text-sm"
+                    style={{ fontFamily: "OpenSans_400Regular" }}
+                  >
+                    +{worseRegress.weightScore}kg
+                  </Text>
+                ) : (
+                  <Text
+                    className="text-red-400 text-sm"
+                    style={{ fontFamily: "OpenSans_400Regular" }}
+                  >
+                    {worseRegress.weightScore}kg
+                  </Text>
+                )}
+              </View>
+            </View>
+          </View>
+        </View>
+        <View
+          style={{ borderRadius: 8 }}
+          className="flex w-full flex-row justify-between bg-[#1E1E1E73] items-center p-4"
+        >
+          <View>
             <Text
               className="text-[#94e798] text-lg"
               style={{ fontFamily: "OpenSans_700Bold" }}
             >
-              Best progress:
+              Progress
             </Text>
-            <Text
-              className="text-white text-base"
-              style={{ fontFamily: "OpenSans_400Regular" }}
-            >
-              {bestProgress.exercise}
-            </Text>
-          </View>
-
-          <View style={{ gap: 8 }} className="flex flex-col  justify-around">
-            <Text
-              className="text-white text-sm"
-              style={{ fontFamily: "OpenSans_400Regular" }}
-            >
-              Series number: {bestProgress.series}
-            </Text>
-            <View style={{ gap: 8 }} className="flex flex-row">
+            <View className="flex flex-col h-36 justify-around">
+              <View className="flex flex-col gap-2">
+                <Text
+                  className="text-white text-sm"
+                  style={{ fontFamily: "OpenSans_400Regular" }}
+                >
+                  Current Rank: {props.trainingSummary.profileRank.name}
+                </Text>
+                <Text
+                  className="text-white text-sm"
+                  style={{ fontFamily: "OpenSans_400Regular" }}
+                >
+                  Elo: (
+                  {props.trainingSummary.userOldElo +
+                    props.trainingSummary.gainElo}
+                  ) (
+                  {props.trainingSummary.gainElo >= 0
+                    ? `+${props.trainingSummary.gainElo}`
+                    : props.trainingSummary.gainElo}
+                  )
+                </Text>
+                <Text
+                  className="text-white text-sm"
+                  style={{ fontFamily: "OpenSans_400Regular" }}
+                >
+                  Next Rank:{" "}
+                  {props.trainingSummary.nextRank
+                    ? props.trainingSummary.nextRank.name
+                    : "You are on the highest rank"}
+                </Text>
+              </View>
+              {props.trainingSummary.nextRank &&
+              props.trainingSummary.nextRank.needElo &&
+              progress ? (
+                <ProgressBar width={progress} />
+              ) : (
+                <Text></Text>
+              )}
               <Text
                 className="text-white text-sm"
                 style={{ fontFamily: "OpenSans_400Regular" }}
               >
-                Reps:
+                Completed: {progress ? progress : <Text></Text>}%
               </Text>
-              {bestProgress.repsScore >= 0 ? (
-                <Text
-                  className="text-[#94e798] text-sm"
-                  style={{ fontFamily: "OpenSans_400Regular" }}
-                >
-                  +{bestProgress.repsScore}
-                </Text>
-              ) : (
-                <Text
-                  className="text-red-400 text-sm"
-                  style={{ fontFamily: "OpenSans_400Regular" }}
-                >
-                  {bestProgress.repsScore}
-                </Text>
-              )}
-            </View>
-            <View style={{ gap: 8 }} className="flex flex-row">
-              <Text
-                className="text-white text-sm"
-                style={{ fontFamily: "OpenSans_400Regular" }}
-              >
-                Weight:
-              </Text>
-              {bestProgress.weightScore >= 0 ? (
-                <Text
-                  className="text-[#94e798] text-sm"
-                  style={{ fontFamily: "OpenSans_400Regular" }}
-                >
-                  +{bestProgress.weightScore}
-                </Text>
-              ) : (
-                <Text
-                  className="text-red-400 text-sm"
-                  style={{ fontFamily: "OpenSans_400Regular" }}
-                >
-                  {bestProgress.weightScore}
-                </Text>
-              )}
             </View>
           </View>
+          <ProfileRank rank={props.trainingSummary.profileRank.name} />
         </View>
-        <View className=" bg-[#1E1E1E73] w-full  p-4">
-          <View style={{ gap: 8 }} className="flex flex-row items-center">
-            <Text
-              className="text-red-400 text-lg"
-              style={{ fontFamily: "OpenSans_700Bold" }}
-            >
-              Worst Regress:{" "}
-            </Text>
-            <Text
-              className="text-white text-base"
-              style={{ fontFamily: "OpenSans_400Regular" }}
-            >
-              {worseRegress.exercise}
-            </Text>
-          </View>
-          <View style={{ gap: 8 }} className="flex flex-col  justify-around">
-            <Text
-              className="text-white text-sm"
-              style={{ fontFamily: "OpenSans_400Regular" }}
-            >
-              Series number: {worseRegress.series}
-            </Text>
-            <View style={{ gap: 8 }} className="flex flex-row">
-              <Text
-                className="text-white text-sm"
-                style={{ fontFamily: "OpenSans_400Regular" }}
-              >
-                Reps:
-              </Text>
-              {worseRegress.repsScore >= 0 ? (
-                <Text
-                  className="text-[#94e798] text-sm"
-                  style={{ fontFamily: "OpenSans_400Regular" }}
-                >
-                  +{worseRegress.repsScore}
-                </Text>
-              ) : (
-                <Text
-                  className="text-red-400 text-sm"
-                  style={{ fontFamily: "OpenSans_400Regular" }}
-                >
-                  {worseRegress.repsScore}
-                </Text>
-              )}
-            </View>
-            <View style={{ gap: 8 }} className="flex flex-row">
-              <Text
-                className="text-white text-sm"
-                style={{ fontFamily: "OpenSans_400Regular" }}
-              >
-                Weight:
-              </Text>
-              {worseRegress.weightScore >= 0 ? (
-                <Text
-                  className="text-[#94e798] text-sm"
-                  style={{ fontFamily: "OpenSans_400Regular" }}
-                >
-                  +{worseRegress.weightScore}
-                </Text>
-              ) : (
-                <Text
-                  className="text-red-400 text-sm"
-                  style={{ fontFamily: "OpenSans_400Regular" }}
-                >
-                  {worseRegress.weightScore}
-                </Text>
-              )}
-            </View>
-          </View>
-        </View>
-      </View>
-      <View
-        style={{ borderRadius: 8 }}
-        className="flex w-full flex-row justify-between bg-[#1E1E1E73] items-center p-4"
-      >
-        <View>
+      </ScrollView>
+      <View className="flex w-full flex-row justify-end">
+        <Pressable
+          onPress={() => props.closePopUp()}
+          style={{ borderRadius: 8 }}
+          className={` flex flex-row justify-center items-center w-28 h-14 bg-[#94e798]`}
+        >
           <Text
-            className="text-[#94e798] text-lg"
-            style={{ fontFamily: "OpenSans_700Bold" }}
+            className="text-base"
+            style={{ fontFamily: "OpenSans_400Regular" }}
           >
-            Progress
+            Continue
           </Text>
-          <View className="flex flex-col h-36 justify-around">
-            <View className="flex flex-col gap-2">
-              <Text
-                className="text-white text-sm"
-                style={{ fontFamily: "OpenSans_400Regular" }}
-              >
-                Current Rank: {props.trainingSummary.profileRank.name}
-              </Text>
-              <Text
-                className="text-white text-sm"
-                style={{ fontFamily: "OpenSans_400Regular" }}
-              >
-                Elo: (
-                {props.trainingSummary.userOldElo +
-                  props.trainingSummary.gainElo}
-                ) (
-                {props.trainingSummary.gainElo >= 0
-                  ? `+${props.trainingSummary.gainElo}`
-                  : props.trainingSummary.gainElo}
-                )
-              </Text>
-              <Text
-                className="text-white text-sm"
-                style={{ fontFamily: "OpenSans_400Regular" }}
-              >
-                Next Rank:{" "}
-                {props.trainingSummary.nextRank
-                  ? props.trainingSummary.nextRank.name
-                  : "You are on the highest rank"}
-              </Text>
-            </View>
-            {props.trainingSummary.nextRank &&
-            props.trainingSummary.nextRank.needElo &&
-            progress ? (
-              <ProgressBar width={progress} />
-            ) : (
-              <Text></Text>
-            )}
-            <Text
-              className="text-white text-sm"
-              style={{ fontFamily: "OpenSans_400Regular" }}
-            >
-              Completed: {progress ? progress : <Text></Text>}%
-            </Text>
-          </View>
-        </View>
-        <ProfileRank rank={props.trainingSummary.profileRank.name} />
+        </Pressable>
       </View>
     </View>
   );
