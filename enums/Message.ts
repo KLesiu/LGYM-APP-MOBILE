0e1f9f9ec1e514d2d2a1bdec59fee9788d215b08
Lesiuuu <j.leskiewicz@tdz.com.pl>
2025-05-29 16:08:57 +0200

    update enums

--- enums/Message.ts
+++ enums/Message.ts
@@ -1,22 +1,19 @@
-export enum Message{
-    Created = 'Created',
-    FieldRequired = 'All fields required',
-    ChooseDays = 'You have to choose days number between 1-7',
-    Updated = 'Updated',
-    TryAgain = 'Error, try again',
-    Deleted = 'Deleted!',
-    DidntFind = 'Didnt find!',
-    InvalidToken = 'Invalid JWT Token',
-    ExpiredToken = 'Token expired',
-    InputsMustBeNumbers = 'Inputs must be numbers',
-    NameIsRequired = "Name is required, and has to have minimum one character",
-    EmailInvalid = "This email is not valid!",
-    PasswordMin = "Passwword need to have minimum six characters",
-    SamePassword = "Passwords need to be same",
-    UserWithThatName = "We have user with that name",
-    UserWithThatEmail = "We have user with that email",
-
-
-
-    
+export enum Message {
+  Created = "Created",
+  FieldRequired = "All fields required",
+  ChooseDays = "You have to choose days number between 1-7",
+  Updated = "Updated",
+  TryAgain = "Error, try again",
+  Deleted = "Deleted!",
+  DidntFind = "Didnt find!",
+  InvalidToken = "Invalid JWT Token",
+  ExpiredToken = "Token expired",
+  InputsMustBeNumbers = "Inputs must be numbers",
+  NameIsRequired = "Name is required, and has to have minimum one character",
+  EmailInvalid = "This email is not valid!",
+  PasswordMin = "Passwword need to have minimum six characters",
+  SamePassword = "Passwords need to be same",
+  UserWithThatName = "We have user with that name",
+  UserWithThatEmail = "We have user with that email",
+  Unauthorized = "Unauthorized",
 }
